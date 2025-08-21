7e2061da26f72b14c7867b0d7b30ac5ec9fd03e2
Pubudu Lakshan <pubuduakshan04@gmail.com>
2025-08-21 08:08:13 +0530

    Update bus_checker.py

--- bus_checker.py
+++ bus_checker.py
@@ -23,7 +23,7 @@ def check_bus_availability():
 
     # Use the webdriver-manager to automatically handle the driver installation
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     
