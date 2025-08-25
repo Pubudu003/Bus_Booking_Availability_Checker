e2a369746a6f800891b17a8d9d7b0cd1cf9a95ee
Pubudu Lakshan <pubuduakshan04@gmail.com>
2025-08-25 15:06:54 +0530

    Update bus_checker.py
    fixing indentation error 1

--- bus_checker.py
+++ bus_checker.py
@@ -70,7 +70,7 @@ def send_email_notification():
 def send_booking_unavailable_email():
     subject = "Bus Booking not available yet!"
     body = f"Bus booking is yet not avalable for, from {FROM_LOCATION} to {TO_LOCATION} on {SEARCH_DATE}.\n\n"
-            f"You can check at https://sltb.eseat.lk/search?type=1&from={FROM_LOCATION}&to={TO_LOCATION}&from_date={SEARCH_DATE}&to_date="
+           f"You can check at https://sltb.eseat.lk/search?type=1&from={FROM_LOCATION}&to={TO_LOCATION}&from_date={SEARCH_DATE}&to_date="
 
     message = f"Subject: {subject}\n\n{body}"
 
