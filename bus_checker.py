85d023c25d360e0ae27be18d86ad6b25d0db30c9
Pubudu Lakshan <pubuduakshan04@gmail.com>
2025-08-30 13:50:06 +0530

    Update bus_checker.py
    import bs4
    add SEAT_CHECKING_ID
    add find_seat_count
    add send_seat_count_email
    update main process

--- bus_checker.py
+++ bus_checker.py
@@ -2,16 +2,21 @@ import smtplib
 import ssl
 import time
 import os
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # --- Configuration ---
-FROM_LOCATION = "Makumbura"
-TO_LOCATION = "Wellawaya"
-SEARCH_DATE = "2025-09-04"
-BUS_SCHEDULE_ID = "SLTTSEX01-2100-CA"
+FROM_LOCATION = "Wellawaya"
+TO_LOCATION = "Makumbura"
+SEARCH_DATE = "2025-09-08"
+BUS_SCHEDULE_ID = "SLTTSEX198-1600-MC"
+SEAT_CHECKING_ID = "870555"
 
 SENDER_EMAIL = "pubuduakshan04@gmail.com"
 SENDER_PASSWORD = os.environ.get("SENDER_PASSWORD")
@@ -47,7 +52,52 @@ def check_bus_availability():
     finally:
         if driver:
             driver.quit()
+
+# --- Find the available bus sheat count ---
+def find_available_bus_seats():
+    search_url = f"https://sltb.eseat.lk/seats/{SEAT_CHECKING_ID}"
     
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    
+    driver = None
+    try:
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
+        print("Opening browser to check bus availability...")
+        driver.get(search_url)
+        
+        print("Waiting for seat information to load...")
+        WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'ul.list-group li.list-group-item'))
+        )
+        
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        elements = soup.select('ul.list-group li.list-group-item')
+        
+        if elements:
+            print(f"found {len(elements)} elements.")
+            available_seat_index = -1
+            for index, element in enumerate(elements):
+                if element.get_text().strip() == "Available Seats":
+                    available_seat_index = index + 1
+                    break
+            available_seats = elements[available_seat_index].get_text().strip()
+            return available_seats
+        else:
+            print("No seat element found!")
+            return False
+            
+    except Exception as e:
+        print(f"Error accessing the website: {e}")
+        return False
+    finally:
+        if driver:
+            driver.quit()
+     
+
 # --- Send an email notification ---
 def send_email_notification():
     subject = "Bus Booking Open!"
@@ -84,12 +134,41 @@ def send_booking_unavailable_email():
     except Exception as e:
         print(f"Failed to send email: {e}")
 
+# --- Send an email notification with available seat count ---
+def send_available_seat_count_email(available_seat_count):
+    subject = "Bus Booking Open and Seats Available!"
+    body = f"Bus booking for the route from {FROM_LOCATION} to {TO_LOCATION} on {SEARCH_DATE} are now available on eseat.lk.\n\n" \
+           f"https://sltb.eseat.lk/seats/{SEAT_CHECKING_ID}" \
+           f"\n\nAvailable Seats: {available_seat_count}"
+           
+    message = f"Subject: {subject}\n\n{body}"
+    
+    context = ssl.create_default_context()
+    
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(SENDER_EMAIL, SENDER_PASSWORD)
+            server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, message)
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+
+
 # --- Main program ---
 if __name__ == "__main__":
     print("Checking for bus availability...")
     if check_bus_availability():
         print("Bus booking is now available!")
-        send_email_notification()
+        print("Checking for bus seat availability...")
+        seat_count = find_available_bus_seats()
+        
+        if seat_count:
+            print(f"Available Seats: {seat_count}")
+            send_available_seat_count_email(seat_count)
+        else:
+            print("No seat information found.")
+            send_email_notification()
+        
     else:
         print("Bus booking is not yet available.")
         send_booking_unavailable_email()
